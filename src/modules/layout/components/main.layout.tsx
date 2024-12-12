4bb22a9f28a5163d7d9a06e6f6243373fe962052
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2024-12-12 10:24:26 -0500

    style: prettier

--- src/modules/layout/components/main.layout.tsx
+++ src/modules/layout/components/main.layout.tsx
@@ -1,13 +1,5 @@
 import { PropsWithChildren } from "react";
 
-export function MainLayout({
-    children,
-}: PropsWithChildren) {
-    return (
-        <main
-            className="max-w-5xl mx-auto"
-        >
-            {children}
-        </main>
-    )
+export function MainLayout({ children }: PropsWithChildren) {
+  return <main className="max-w-5xl mx-auto">{children}</main>;
 }
